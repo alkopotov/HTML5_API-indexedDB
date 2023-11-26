44735c7333a724264233a113d86f01b6d3060727
AlHobb <118611357+alkopotov@users.noreply.github.com>
2023-11-26 12:41:05 +0300

    Console sweeped

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ dbRequest.onupgradeneeded = (e) => {
 
 dbRequest.onsuccess = (e) => {
   db = e.target.result;
-  console.log("БД успешно открыта", db);
   displayRecords(db)
 }
 
@@ -70,7 +69,7 @@ function updateRecord(db, record){
 
   tx.oncomplete = () => {
     alert(`Запись с ключом ${keyRecord} успешно обновлена, новое значение - ${valueRecord}`)
-    console.log('Запись добавлена');
+
     displayRecords(db)
   }
 
@@ -90,7 +89,6 @@ function deleteRecord(db, key){
 
   tx.oncomplete = () => {
     alert(`Запись c ключом ${key} успешно удалена`)
-    console.log('Запись добавлена');
     displayRecords(db)
   }
 
@@ -164,7 +162,6 @@ function createTable(records) {
     deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="delete_button"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>`
 
     newRow.append(keyRow, valueRow, dateTimeRecord, updateButton, deleteButton)
-    console.log(updateButton.parentElement.firstElementChild.textContent)
     updateButton.onclick = (e) => {
       let updatedRecord = {
                             keyRecord: e.currentTarget.parentElement.firstElementChild.textContent,
